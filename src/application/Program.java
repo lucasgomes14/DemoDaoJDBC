6e8aca18107dc935a9a7c89a83ee695a8c42dbf6
lucasgomes14 <lucazmatehus14@gmail.com>
2024-04-08 20:43:47 -0300

    update in error

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,11 +10,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000., obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); // faz que o programa não conhece a implementação, e sim apenas a interface
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
