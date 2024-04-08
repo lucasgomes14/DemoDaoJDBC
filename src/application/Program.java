a68cc23aceb8718096797e3c7ed9cda28cde7f04
lucasgomes14 <lucazmatehus14@gmail.com>
2024-04-08 20:52:52 -0300

    creating helper methods to refactor code

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,6 +12,7 @@ public class Program {
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao(); // faz que o programa não conhece a implementação, e sim apenas a interface
 		
+		System.out.println("===== TEST 1: seller findById =====");		
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
