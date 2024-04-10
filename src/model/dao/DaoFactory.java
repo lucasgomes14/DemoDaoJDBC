91d39c3dce6a1c4e4b56460f613797a5af989c87
lucasgomes14 <lucazmatehus14@gmail.com>
2024-04-10 19:25:03 -0300

    created 'findById in departmentDaoJDBC' method

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -8,7 +9,7 @@ public class DaoFactory {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
-	/*public static DepartmentDao createDepartmentDao() {
-		return new
-	}*/
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
